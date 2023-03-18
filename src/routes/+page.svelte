3fa845c26448fe458c6b9441c01f8645b8c66f0c
Ashay Changwani <ashaychangwani@gmail.com>
2023-03-17 23:33:06 -0400

    First commit + set up + 1 reusable comp

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Title from "$lib/title.svelte";
+</script>
+
+<Title/>
+
+<div class="index">
+    <h2>Ninja Gaming Guides</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel illum nesciunt impedit dolorum facere? Recusandae esse in deleniti. Consequuntur exercitationem perspiciatis doloribus architecto alias delectus fuga distinctio unde id facere.</p> 
+    <a href='/guides'>Guides</a>
+    <a href='/about'>About</a>
+</div>
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
