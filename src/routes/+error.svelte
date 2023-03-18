96123803dc2d7e485db8a99d23b0a49122946d31
Ashay Changwani <ashaychangwani@gmail.com>
2023-03-18 02:48:01 -0400

    added error page for /abc

---
+++ src/routes/+error.svelte
@@ -0,0 +1,36 @@
+<script>
+  import '../styles/global.css'
+  import Title from "$lib/title.svelte";
+</script>
+
+<header>
+  <Title title = "Ninja Gaming Guides"/>
+</header>
+
+<main>
+  <div class="error">
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+</main>
+
+<footer>
+  <p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
+
+
+<style>
+  header {
+      display: flex;
+      justify-content: center;
+  }
+  main {
+      max-width: 960px;
+      margin: 20px auto;
+  }
+
+  footer {
+      text-align: center;
+  }
+
+</style>
