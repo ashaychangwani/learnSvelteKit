14848437e5051421ad7f6391438ec8e20d161025
Ashay Changwani <ashaychangwani@gmail.com>
2023-03-18 00:15:53 -0400

    Created Layout reset, used (groups)

---
+++ src/routes/(grp2)/guides/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+    import '../../../styles/global.css'
+    import Title from "$lib/title.svelte";
+</script>
+
+<header>
+    <Title title = "Ninja Gaming Guidessss"/>
+    <div class="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/guides">Guides</a>
+    </div>
+</header>
+
+<main>
+    <!-- page content -->
+    <slot></slot>
+</main>
+
+<footer>
+    <p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
+
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+    }
+    main {
+        max-width: 960px;
+        margin: 20px auto;
+    }
+    .links {
+        margin-left: auto;
+    }
+    footer {
+        text-align: center;
+    }
+
+</style>
