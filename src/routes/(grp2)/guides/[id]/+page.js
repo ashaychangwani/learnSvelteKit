e254898794fa91912303814c54a515f70b85f40f
Ashay Changwani <ashaychangwani@gmail.com>
2023-03-18 02:48:14 -0400

    added error page for /guides/id/abc

--- src/routes/(grp2)/guides/[id]/+page.js
+++ src/routes/(grp2)/guides/[id]/+page.js
@@ -1,7 +1,7 @@
-import { redirect } from '@sveltejs/kit';
+import { redirect, error } from '@sveltejs/kit';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise(resolve => setTimeout(resolve, 500));
     const id = params.id
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     let guide = [];
@@ -11,6 +11,7 @@ export async function load({ fetch, params }) {
         guide: guide
       }
     }
-
-    throw redirect(301,'/guides');
+    throw error(404, {
+      message: 'This guide could not be found'
+    });
   }
